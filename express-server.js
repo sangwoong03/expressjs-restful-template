45f91e70a64011b33f4f9051c5f7abdb1e1442b1
sangwoong <sangwoong03@gmail.com>
2022-04-08 17:02:02 +0900

    [node. js] main.js

--- express-server.js
+++ express-server.js
@@ -1,21 +1,35 @@
+// express 기본 변수 저장
 const express = require("express");
+const app = express();
+const port = 3000; // port number : ~ 60000
+
 const nunjucks = require("nunjucks");
 const logger = require("morgan");
+const bodyParser = require("body-parser"); // express의 내장 모듈이기 때문에 따로 설치할 필요가 없음.
 
 // 라우터 모듈 불러오기
 const admin = require("./routes/Admin");
 const contact = require("./routes/Contacts");
-
-const app = express();
-const port = 3000;
 
 nunjucks.configure("template", {
 	autoescape: true,
 	express: app,
 });
 
-// middleware setting
+// API middleware setting
 app.use(logger("dev"));
+// body-parser (middle-ware)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// static file
+app.use("/uploads", express.static("uploads"));
+// Global view variable
+app.use((req, res, next) => {
+	app.locals.isLogin = true; // 전역변수 isLogin을 어디서든 접근할 수 있음.
+	app.locals.req_path = req.path; // express에서 현재 url을 보내줌.
+	next();
+});
+
 // 우선순위가 높은 미들웨어
 function primeMiddleware(req, res, next) {
 	console.log("최우선 미들웨어");
@@ -34,14 +48,14 @@ app.get("/sangwoong", (req, res) => {
 app.use("/admin", primeMiddleware, admin);
 app.use("/contact", contact);
 
+app.use((req, res, next) => {
+	res.status(400).render("common/404.html");
+});
+app.use((req, res, next) => {
+	res.status(500).render("common/500.html");
+});
+
+// 웹 서버 실행
 app.listen(port, () => {
 	console.log("Express Listening on port", port);
-}); // 웹 서버를 띄우는 createServer 역할
-
-// npm i express
-// node (file name).js
-
-// It's annoying that I have to reopen server whenever I edit file
-// So I can use tihs command:
-// npm i -g nodemon
-//"nodemon (file name).js" ... node monitoring
+});
