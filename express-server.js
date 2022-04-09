5fadacbb71b98debebe325e55d1b084a98daef81
sangwoong <sangwoong03@gmail.com>
2022-04-09 23:51:05 +0900

    [Node.js] docs: main.js (added codes connecting MongoDB)

--- express-server.js
+++ express-server.js
@@ -7,9 +7,35 @@ const nunjucks = require("nunjucks");
 const logger = require("morgan");
 const bodyParser = require("body-parser"); // express의 내장 모듈이기 때문에 따로 설치할 필요가 없음.
 
+const MongoClient = require("mongodb").MongoClient; // mongoDB 연동하기
+let db;
+MongoClient.connect(
+	"mongodb+srv://sangwoong03:rlatkd6795@clustersw.kcid2.mongodb.net/woong_app?retryWrites=true&w=majority",
+	(err, client) => {
+		// 에러처리
+		if (err) return console.log("err");
+
+		// woong_app이라는 db로 연결
+		db = client.db("woong_app");
+
+		// post라는 폴더에 data 저장
+		// 새로고침 or 저장할 때마다 데이터베이스에 저장됨;;
+		// db.collection("post").insertOne(
+		// 	{ 이름: "sangwoong", 나이: 27 },
+		// 	(err, result) => {
+		// 		console.log("저장완료");
+		// 	},
+		// );
+
+		app.listen(port, () => {
+			console.log("Express Listening on port", port);
+		});
+	},
+);
+
 // 라우터 모듈 불러오기
 const admin = require("./routes/Admin");
-const contact = require("./routes/Contacts");
+const contact = require("./routes/contacts");
 
 nunjucks.configure("template", {
 	autoescape: true,
@@ -27,14 +53,16 @@ app.use("/uploads", express.static("uploads"));
 app.use((req, res, next) => {
 	app.locals.isLogin = true; // 전역변수 isLogin을 어디서든 접근할 수 있음.
 	app.locals.req_path = req.path; // express에서 현재 url을 보내줌.
+	app.locals.user_name = req.name;
+	app.locals.user_number = req.phone_number;
 	next();
 });
 
 // 우선순위가 높은 미들웨어
-function primeMiddleware(req, res, next) {
-	console.log("최우선 미들웨어");
-	next();
-}
+// function primeMiddleware(req, res, next) {
+// 	console.log("최우선 미들웨어");
+// 	next();
+// }
 
 app.get("/", (req, res) => {
 	res.send("hello express");
@@ -45,8 +73,23 @@ app.get("/sangwoong", (req, res) => {
 });
 
 //라우터 모듈 연동하기
-app.use("/admin", primeMiddleware, admin);
+app.use("/admin", admin);
 app.use("/contact", contact);
+
+// 라우터 모듈을 미들웨어로 불러오고 다음 실행 함수로 DB저장하기
+// _id 값의 경우 입력하지 않으면 object("문자문자문자~~")와 같은 임의 값 저장 됨.
+app.post("/add", admin, (req, res) => {
+	db.collection("post").insertOne(
+		{
+			이름: req.body.name,
+			전화번호: req.body.phone_number,
+			_id: req.body.user_id,
+		},
+		(err, client) => {
+			console.log("저장완료");
+		},
+	);
+});
 
 app.use((req, res, next) => {
 	res.status(400).render("common/404.html");
@@ -56,6 +99,6 @@ app.use((req, res, next) => {
 });
 
 // 웹 서버 실행
-app.listen(port, () => {
-	console.log("Express Listening on port", port);
-});
+// app.listen(port, () => {
+// 	console.log("Express Listening on port", port);
+// });
