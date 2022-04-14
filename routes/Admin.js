949babbe7a50301e21cc2e8a1160e2bea7e71834
sangwoong <sangwoong03@gmail.com>
2022-04-14 16:59:19 +0900

    [Node.Js] Router (added get method for login function)

--- routes/Admin.js
+++ routes/Admin.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const router = express.Router(); // express 라우터 분리
+const router = express.Router({ mergeParams: true }); // express 라우터 분리
 // express 라우터를 분리하여 라우터 모듈 작성
+// mergeParams >> 부모 라우터의 파라미터 상속
 
 // middleware module
 // function testMiddleware(req, res, next) {
@@ -14,7 +15,7 @@ const router = express.Router(); // express 라우터 분리
 
 // rotuer "/" url로 이동 >> testMiddleware1, 2를 거치고 >> router res.send 실행
 router.get("/", (req, res) => {
-	res.send("lists 페이지 및 lists/write 페이지로 이동할 수 있습니다.");
+	res.send("Main Page");
 });
 
 router.get("/lists", (req, res) => {
@@ -31,7 +32,15 @@ router.get("/edit/:id", (req, res) => {
 	res.render("admin/edit.html");
 });
 
-//POST
+router.get("/login", (req, res) => {
+	res.render("admin/login.html");
+});
+
+// router.get("/mypage", (req, res) => {
+// 	console.log(req.params.user);
+// 	res.render("admin/mypage.html");
+// });
+
 router.post("/add", (req, res) => {
 	res.redirect("/admin/lists");
 });
