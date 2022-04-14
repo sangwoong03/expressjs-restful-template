e6ae11a58c2a102ee5abef03dc4acd1e0387782a
sangwoong <sangwoong03@gmail.com>
2022-04-14 16:58:32 +0900

    [Node.Js] Main (express-session, passport, passport-local)

--- express-server.js
+++ express-server.js
@@ -1,13 +1,27 @@
 // express 기본 변수 저장
 const express = require("express");
 const app = express();
-const port = 3000; // port number : ~ 60000
+const port = 5000; // port number : ~ 60000
 
 const nunjucks = require("nunjucks");
 const logger = require("morgan");
 const bodyParser = require("body-parser"); // express의 내장 모듈이기 때문에 따로 설치할 필요가 없음.
 const methodOverride = require("method-override");
 app.use(methodOverride("_method"));
+
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+app.use(
+	session({
+		secret: "secret",
+		resave: true,
+		saveUninitialized: false,
+		cookie: { httpOnly: true, secure: false },
+	}),
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 const MongoClient = require("mongodb").MongoClient; // mongoDB 연동하기
 let db;
@@ -64,12 +78,11 @@ app.use((req, res, next) => {
 // }
 
 app.get("/", (req, res) => {
-	res.send(
-		"Hello, This is main page of the Node.js and MongoDB tutorial project. You can move to '/admin', '/admin/lists', and '/admin/lists/write' pages",
-	);
+	res.send("Home");
 }); // url  (메인 url에서 마지막 / 유무 상관 없음)
 
 //라우터 모듈 연동하기
+// Fetch
 function fetchData(req, res) {
 	db.collection("post")
 		.find()
@@ -79,8 +92,11 @@ function fetchData(req, res) {
 }
 app.get("/admin/lists", fetchData);
 app.use("/admin", admin);
+
 // 라우터 모듈을 미들웨어로 불러오고 다음 실행 함수로 DB저장하기
 // _id 값의 경우 입력하지 않으면 object("문자문자문자~~")와 같은 임의 값 저장 됨.
+
+// Add
 function addData(req, res, next) {
 	db.collection("counter").findOne(
 		{ name: "numOfLists" },
@@ -115,6 +131,7 @@ function addData(req, res, next) {
 
 app.post("/add", addData, admin);
 
+// Delete
 app.delete(
 	"/delete",
 	(req, res, next) => {
@@ -134,6 +151,7 @@ app.delete(
 	admin,
 );
 
+// Edit
 app.get("/edit/:id", (req, res, next) => {
 	const urlId = parseInt(req.params.id);
 	db.collection("post").findOne({ _id: urlId }, (err, result) => {
@@ -159,6 +177,69 @@ app.put(
 	},
 	admin,
 );
+
+// Login
+app.post(
+	"/login",
+	passport.authenticate("local", {
+		failureRedirect: "/fail",
+	}),
+	(req, res) => {
+		res.redirect("/");
+	},
+);
+
+function confirmLogin(req, res, next) {
+	if (req.user) {
+		next();
+	} else {
+		console.log("no user");
+	}
+}
+app.get("/mypage", confirmLogin, (req, res) => {
+	console.log(req.user);
+	res.render("admin/mypage.html");
+});
+
+passport.use(
+	new LocalStrategy(
+		{
+			usernameField: "email",
+			passwordField: "pw",
+			session: true,
+			passReqToCallback: true,
+		},
+		// DB와 회원정보 검증
+		function (email, pw, done) {
+			//console.log(입력한아이디, 입력한비번);
+			db.collection("login").findOne({ email: email }, function (err, result) {
+				if (err) return done(result);
+
+				if (!result)
+					return done(null, false, { message: "존재하지않는 아이디요" });
+				// done(서버에러, 성공 시 사용자 DB데이터, 에러메시지)
+				if (pw == result.pw) {
+					return done(null, result);
+				} else {
+					return done(null, false, { message: "비번틀렸어요" });
+				}
+			});
+		},
+	),
+);
+
+// email을 이용해서 세션을 저장시키는 코드 (로그인 성공 시 함수 실행)
+passport.serializeUser(function (user, done) {
+	done(null, user.email);
+});
+
+// 세션 데이터를 가진 정보를 DB에서 찾는 함수 (eg.마이페이지 접속 시 실행)
+// {} 정보를 객체로 출력할 수 있음.
+passport.deserializeUser(function (email, done) {
+	db.collection("login").findOne({ email: email }, (err, result) => {
+		done(null, result);
+	});
+});
 
 app.use((req, res, next) => {
 	res.status(400).render("common/404.html");
