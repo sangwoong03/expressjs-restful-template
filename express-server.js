ee11e5f1991442fa14edfec011f4595aa607c418
sangwoong <sangwoong03@gmail.com>
2022-04-17 20:50:48 +0900

    [Node.Js] docs: Main ( refactoring... module for login)

--- express-server.js
+++ express-server.js
@@ -10,20 +10,6 @@ const bodyParser = require("body-parser"); // express의 내장 모듈이기 때
 const methodOverride = require("method-override");
 app.use(methodOverride("_method"));
 
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const session = require("express-session");
-app.use(
-	session({
-		secret: "secret",
-		resave: true,
-		saveUninitialized: false,
-		cookie: { httpOnly: true, secure: false },
-	}),
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
 nunjucks.configure("template", {
 	autoescape: true,
 	express: app,
@@ -31,6 +17,7 @@ nunjucks.configure("template", {
 
 // 라우터 모듈 불러오기
 const admin = require("./routes/Admin");
+const login = require("./routes/Login");
 
 // API middleware setting
 app.use(logger("dev"));
@@ -46,83 +33,23 @@ app.use((req, res, next) => {
 	next();
 });
 
-// 우선순위가 높은 미들웨어
-// function primeMiddleware(req, res, next) {
-// 	console.log("최우선 미들웨어");
-// 	next();
-// }
-
 app.get("/", (req, res) => {
-	res.send("Home");
+	res.redirect("/login");
 }); // url  (메인 url에서 마지막 / 유무 상관 없음)
 
+// router
 app.use("/admin", admin);
+app.use("/login", login);
+
+// admin
 app.post("/add", admin);
 app.delete("/delete", admin);
 app.get("/edit/:id", admin);
 app.put("/edit", admin);
+
 // Login
-// app.post(
-// 	"/login",
-// 	passport.authenticate("local", {
-// 		failureRedirect: "/fail",
-// 	}),
-// 	(req, res) => {
-// 		res.redirect("/");
-// 	},
-// );
-
-// function confirmLogin(req, res, next) {
-// 	if (req.user) {
-// 		next();
-// 	} else {
-// 		console.log("no user");
-// 	}
-// }
-// app.get("/mypage", confirmLogin, (req, res) => {
-// 	console.log(req.user);
-// 	res.render("admin/mypage.html");
-// });
-
-// passport.use(
-// 	new LocalStrategy(
-// 		{
-// 			usernameField: "email",
-// 			passwordField: "pw",
-// 			session: true,
-// 			passReqToCallback: true,
-// 		},
-// 		// DB와 회원정보 검증
-// 		function (email, pw, done) {
-// 			//console.log(입력한아이디, 입력한비번);
-// 			db.collection("login").findOne({ email: email }, function (err, result) {
-// 				if (err) return done(result);
-
-// 				if (!result)
-// 					return done(null, false, { message: "존재하지않는 아이디요" });
-// 				// done(서버에러, 성공 시 사용자 DB데이터, 에러메시지)
-// 				if (pw == result.pw) {
-// 					return done(null, result);
-// 				} else {
-// 					return done(null, false, { message: "비번틀렸어요" });
-// 				}
-// 			});
-// 		},
-// 	),
-// );
-
-// email을 이용해서 세션을 저장시키는 코드 (로그인 성공 시 함수 실행)
-// passport.serializeUser(function (user, done) {
-// 	done(null, user.email);
-// });
-
-// 세션 데이터를 가진 정보를 DB에서 찾는 함수 (eg.마이페이지 접속 시 실행)
-// {} 정보를 객체로 출력할 수 있음.
-// passport.deserializeUser(function (email, done) {
-// 	db.collection("login").findOne({ email: email }, (err, result) => {
-// 		done(null, result);
-// 	});
-// });
+app.post("/login", login);
+app.get("/mypage", login);
 
 app.use((req, res, next) => {
 	res.status(400).render("common/404.html");
