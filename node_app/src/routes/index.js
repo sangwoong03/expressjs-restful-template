d507aa428cfb51e45cc08de5baffc71c9c23fe19
sangwoong <sangwoong03@gmail.com>
2022-12-20 23:44:45 +0900

    feat: Added test codes for user-api

--- node_app/src/routes/index.js
+++ node_app/src/routes/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const userRouter = require("./userRouter");
-const postRouter = require("./postRouter");
-const likeRouter = require("./likeRouter");
+// const postRouter = require("./postRouter");
+// const likeRouter = require("./likeRouter");
 
 router.use("/users", userRouter.router);
-router.use("/posts", postRouter.router);
-router.use("/likes", likeRouter.router);
+// router.use("/posts", postRouter.router);
+// router.use("/likes", likeRouter.router);
 
 module.exports = router;
