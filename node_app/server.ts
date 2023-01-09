f80ddacba100a5de41b5b2d0c41135626f0a502f
sangwoong <sangwoong03@gmail.com>
2023-01-09 23:47:18 +0900

    feat: Initialized for TypeScript Exrpess server tutorial

---
+++ node_app/server.ts
@@ -0,0 +1,36 @@
+import "dotenv/config"
+import express, {
+  Express,
+  Request,
+  Response,
+  NextFunction
+} from 'express'
+
+import App from "./app"
+
+const startServer = async () => {
+  const port : number = (process.env.PORT! || 8080) as number
+  const app  : App    = new App()
+  const initializeApp = await app.createApp()
+  const initializeDB  = await app.getDBConnection()
+
+  const errorHandler  = (err: any, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack)
+
+    res.status(500).send("Error: " + err.message)
+  }
+	
+  initializeApp.listen(port, () => {
+    console.log(`LISTENING_TO_${port}`);
+  });
+
+  initializeDB.initialize()
+    .then(() => {
+      console.log("Initialized Database Succesfully")
+    })
+    .catch(err => {
+      console.error("Failed to connecting Database " + err )
+    })
+};
+
+startServer();
